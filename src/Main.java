344f40c0224554758eac70edaf9ed3ec21fcb74f
Aitor C. Figueroa <aitorcf@gmail.com>
2022-11-20 16:59:00 +0100

    Ejercicio 2 done

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        Perro perro1 = new Perro();
+        perro1.pelo = "corto";
+        perro1.color = "negro";
+        perro1.chip = 1;
     }
 }
